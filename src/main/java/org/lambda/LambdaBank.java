c022d8379442ef8ca599f9e0d7b7017194ad4373
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2024-02-06 21:01:15 -0300

    quota amount calculated and returning response

--- src/main/java/org/lambda/LambdaBank.java
+++ src/main/java/org/lambda/LambdaBank.java
@@ -40,13 +40,52 @@ public class LambdaBank implements RequestHandler<BankRequest, BankResponse> {
 
         Integer term = bankRequest.getTerm();
 
-        return null;
+        BigDecimal monthlyPayment = this.calculateQuotaAmount(amount, monthlyRate, term, mathContext);
+        BigDecimal monthlyPaymentWithAcc = this.calculateQuotaAmount(amount, monthlyRateWithAcc, term, mathContext);
+
+        BankResponse bankResponse = new BankResponse();
+
+        bankResponse.setQuota(monthlyPayment);
+        bankResponse.setRate(monthlyRate);
+        bankResponse.setTerm(term);
+        bankResponse.setQuotaWithAcc(monthlyPaymentWithAcc);
+        bankResponse.setRateWithAcc(monthlyRateWithAcc);
+        bankResponse.setTermWithAcc(term);
+
+        return bankResponse;
     }
 
     /*
         Función para calcular el monto de las cuotas
+         * P = Monto del préstamo (está en el Request [amount])
+         * i = Tasa de interés mensual (está en el Request [rate])
+         * n = Plazo del crédito en meses (está en el Request [term])
+         *
+         * Cuota mensual = (P * i) / (1 - (1 + i) ^ (-n))
+         * Se restan 0.2% de interés si el solicitante tiene cuenta bancaria en la entidad
      */
-    public BigDecimal calculateQuotaAmount() {
-        return null;
+    public BigDecimal calculateQuotaAmount(BigDecimal amount, BigDecimal monthlyRate, Integer term, MathContext mathContext) {
+
+        // (1 + i)
+        BigDecimal onePlusRate = monthlyRate.add(BigDecimal.ONE, mathContext);
+
+        // (1 + i) ^ n
+        BigDecimal onePlusRateToN = onePlusRate.pow(term, mathContext);
+
+        // (1 + i) ^ -n (Inverso multiplicativo)
+        BigDecimal onePlusRateToNegativeN = BigDecimal.ONE.divide(onePlusRateToN, mathContext);
+
+        // (1 - (1 + i) ^ (-n)
+        BigDecimal subtractOneFromPow = BigDecimal.ONE.subtract(onePlusRateToNegativeN, mathContext);
+
+        // (P * i)
+        BigDecimal finalRate = amount.multiply(monthlyRate, mathContext);
+
+        // (P * i) / (1 - (1 + i) ^ (-n))
+        BigDecimal finalOperation = finalRate.divide(subtractOneFromPow, mathContext);
+
+        finalOperation = finalOperation.setScale(2, RoundingMode.HALF_UP);
+
+        return finalOperation;
     }
 }
